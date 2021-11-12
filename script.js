3c6f8c0533e499e9e447898ea343ab96704e17ac
Dan <drblack651@gmail.com>
2021-11-11 22:20:19 -0600

    more content and styling

---
+++ script.js
@@ -0,0 +1,10 @@
+// const kenobi = document.getElementById('kenobi');
+// const kenobiText = document.getElementById('helloThere');
+
+// kenobiText.addEventListener('mouseover', () => {
+//   kenobi.classList.toggle('hide'); 
+// });
+
+// kenobiText.addEventListener('mouseout', () => {
+//   kenobi.classList.toggle('hide'); 
+// });
